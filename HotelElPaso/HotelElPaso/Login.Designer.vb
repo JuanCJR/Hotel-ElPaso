5e03b0678c6003d7bbaa82fb40f1d22e06a309fc
Fernando Junco <47456887+yokian1@users.noreply.github.com>
2019-02-08 15:48:49 -0400

    Add files via upload

--- HotelElPaso/HotelElPaso/Login.Designer.vb
+++ HotelElPaso/HotelElPaso/Login.Designer.vb
@@ -22,54 +22,126 @@ Partial Class Login
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.iniciar = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ingresar = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.borrar = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
-        'Button1
+        'iniciar
         '
-        Me.Button1.Location = New System.Drawing.Point(39, 313)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Iniciar"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.iniciar.Location = New System.Drawing.Point(48, 262)
+        Me.iniciar.Name = "iniciar"
+        Me.iniciar.Size = New System.Drawing.Size(106, 48)
+        Me.iniciar.TabIndex = 0
+        Me.iniciar.Text = "Iniciar"
+        Me.iniciar.UseVisualStyleBackColor = True
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(402, 343)
+        Me.Label1.Location = New System.Drawing.Point(436, 347)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.Size = New System.Drawing.Size(104, 13)
         Me.Label1.TabIndex = 1
-        Me.Label1.Text = "Label1"
+        Me.Label1.Text = "Esperando conexion"
         '
-        'Button2
+        'ingresar
         '
-        Me.Button2.Location = New System.Drawing.Point(93, 47)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
-        Me.Button2.TabIndex = 2
-        Me.Button2.Text = "Button2"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.ingresar.Location = New System.Drawing.Point(222, 262)
+        Me.ingresar.Name = "ingresar"
+        Me.ingresar.Size = New System.Drawing.Size(104, 48)
+        Me.ingresar.TabIndex = 2
+        Me.ingresar.Text = "Ingresar"
+        Me.ingresar.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Location = New System.Drawing.Point(39, 121)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(455, 100)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Inicio de sesion"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 30)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(46, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Usuario:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 69)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(64, 13)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Contraseña:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(83, 27)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(288, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(83, 66)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.TextBox2.Size = New System.Drawing.Size(288, 20)
+        Me.TextBox2.TabIndex = 3
+        '
+        'borrar
+        '
+        Me.borrar.Location = New System.Drawing.Point(387, 262)
+        Me.borrar.Name = "borrar"
+        Me.borrar.Size = New System.Drawing.Size(107, 48)
+        Me.borrar.TabIndex = 4
+        Me.borrar.Text = "Borrar"
+        Me.borrar.UseVisualStyleBackColor = True
         '
         'Login
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(566, 384)
-        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.borrar)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.ingresar)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.iniciar)
         Me.Name = "Login"
         Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents Button1 As Button
+    Friend WithEvents iniciar As Button
     Friend WithEvents Label1 As Label
-    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents ingresar As System.Windows.Forms.Button
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents borrar As System.Windows.Forms.Button
 End Class
--- HotelElPaso/HotelElPaso/Login.vb
+++ HotelElPaso/HotelElPaso/Login.vb
@@ -1,6 +1,6 @@
 ﻿Imports MySql.Data.MySqlClient
 Public Class Login
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles iniciar.Click
 
         'Consulta Sql para el inicio de sesion'
         Dim consulta As String = "select nombre from usuarios"
@@ -24,7 +24,21 @@ Public Class Login
 
     End Sub
 
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        MsgBox("Culo Grande")
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles ingresar.Click
+        Dim consulta As String = "select nombre, clave from usuarios where nombre='" & TextBox1.Text & "' and clave = '" & TextBox2.Text & "'"
+    End Sub
+
+    Private Sub borrar_Click(sender As Object, e As EventArgs) Handles borrar.Click
+        TextBox1.Clear()
+        TextBox2.Clear()
+        TextBox1.Focus()
+    End Sub
+
+    Private Sub Login_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        Dim x As MsgBoxResult
+        x = MsgBox("¿Desea salir del programa?", 4 + 32)
+        If x = MsgBoxResult.No Then
+            e.Cancel = True
+        End If
     End Sub
 End Class
