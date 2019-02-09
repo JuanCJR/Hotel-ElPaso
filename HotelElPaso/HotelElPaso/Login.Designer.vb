10ef64899ffb0405e5cdbd0bfc47a743b0d566ea
Fernando Junco <47456887+yokian1@users.noreply.github.com>
2019-02-08 20:22:49 -0400

    Add files via upload

--- HotelElPaso/HotelElPaso/Login.Designer.vb
+++ HotelElPaso/HotelElPaso/Login.Designer.vb
@@ -23,65 +23,55 @@ Partial Class Login
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.iniciar = New System.Windows.Forms.Button()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.ingresar = New System.Windows.Forms.Button()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.borrar = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'iniciar
         '
-        Me.iniciar.Location = New System.Drawing.Point(48, 262)
+        Me.iniciar.Location = New System.Drawing.Point(48, 298)
         Me.iniciar.Name = "iniciar"
         Me.iniciar.Size = New System.Drawing.Size(106, 48)
         Me.iniciar.TabIndex = 0
         Me.iniciar.Text = "Iniciar"
         Me.iniciar.UseVisualStyleBackColor = True
         '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(436, 347)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(104, 13)
-        Me.Label1.TabIndex = 1
-        Me.Label1.Text = "Esperando conexion"
-        '
-        'ingresar
-        '
-        Me.ingresar.Location = New System.Drawing.Point(222, 262)
-        Me.ingresar.Name = "ingresar"
-        Me.ingresar.Size = New System.Drawing.Size(104, 48)
-        Me.ingresar.TabIndex = 2
-        Me.ingresar.Text = "Ingresar"
-        Me.ingresar.UseVisualStyleBackColor = True
-        '
         'GroupBox1
         '
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.ComboBox1)
         Me.GroupBox1.Controls.Add(Me.TextBox2)
         Me.GroupBox1.Controls.Add(Me.TextBox1)
         Me.GroupBox1.Controls.Add(Me.Label3)
         Me.GroupBox1.Controls.Add(Me.Label2)
-        Me.GroupBox1.Location = New System.Drawing.Point(39, 121)
+        Me.GroupBox1.Location = New System.Drawing.Point(20, 121)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(455, 100)
+        Me.GroupBox1.Size = New System.Drawing.Size(499, 155)
         Me.GroupBox1.TabIndex = 3
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Inicio de sesion"
         '
-        'Label2
+        'TextBox2
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(6, 30)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(46, 13)
-        Me.Label2.TabIndex = 0
-        Me.Label2.Text = "Usuario:"
+        Me.TextBox2.Location = New System.Drawing.Point(98, 66)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.TextBox2.Size = New System.Drawing.Size(288, 20)
+        Me.TextBox2.TabIndex = 3
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(98, 27)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(288, 20)
+        Me.TextBox1.TabIndex = 2
         '
         'Label3
         '
@@ -92,29 +82,41 @@ Partial Class Login
         Me.Label3.TabIndex = 1
         Me.Label3.Text = "Contraseña:"
         '
-        'TextBox1
+        'Label2
         '
-        Me.TextBox1.Location = New System.Drawing.Point(83, 27)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(288, 20)
-        Me.TextBox1.TabIndex = 2
-        '
-        'TextBox2
-        '
-        Me.TextBox2.Location = New System.Drawing.Point(83, 66)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
-        Me.TextBox2.Size = New System.Drawing.Size(288, 20)
-        Me.TextBox2.TabIndex = 3
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 30)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(46, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Usuario:"
         '
         'borrar
         '
-        Me.borrar.Location = New System.Drawing.Point(387, 262)
+        Me.borrar.Location = New System.Drawing.Point(327, 298)
         Me.borrar.Name = "borrar"
         Me.borrar.Size = New System.Drawing.Size(107, 48)
         Me.borrar.TabIndex = 4
         Me.borrar.Text = "Borrar"
         Me.borrar.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"EMPLEADO", "ADMIN"})
+        Me.ComboBox1.Location = New System.Drawing.Point(98, 108)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(0, 111)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(95, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Tipo de empleado:"
         '
         'Login
         '
@@ -123,25 +125,22 @@ Partial Class Login
         Me.ClientSize = New System.Drawing.Size(566, 384)
         Me.Controls.Add(Me.borrar)
         Me.Controls.Add(Me.GroupBox1)
-        Me.Controls.Add(Me.ingresar)
-        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.iniciar)
         Me.Name = "Login"
         Me.Text = "Form1"
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents iniciar As Button
-    Friend WithEvents Label1 As Label
-    Friend WithEvents ingresar As System.Windows.Forms.Button
     Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
     Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
     Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
     Friend WithEvents Label3 As System.Windows.Forms.Label
     Friend WithEvents Label2 As System.Windows.Forms.Label
     Friend WithEvents borrar As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
 End Class
--- HotelElPaso/HotelElPaso/Login.vb
+++ HotelElPaso/HotelElPaso/Login.vb
@@ -1,10 +1,14 @@
 ﻿Imports MySql.Data.MySqlClient
 Public Class Login
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles iniciar.Click
-
+        Dim usuario As String
+        Dim clave As Integer
+        Dim tipo_usu As String
+        usuario = TextBox1.Text
+        clave = TextBox2.Text
+        tipo_usu = ComboBox1.Text
         'Consulta Sql para el inicio de sesion'
-        Dim consulta As String = "select nombre from usuarios"
-
+        Dim consulta As String = "select * from usuarios where nombre='" & usuario & "' and clave =" & clave & " and tipo_usu ='" & tipo_usu & "';"
 
         'llama procedmiento que Realiza conexion a mysql' 
         Conectar()
@@ -16,16 +20,26 @@ Public Class Login
 
         'Validador de ejecucion de la consulta'
         If dr.Read() = True Then
+            If tipo_usu = "EMPLEADO" Then
+                Hotel_El_paso_inicio.Show()
+                Hotel_El_paso_inicio.AdministracionToolStripMenuItem.Available = False
+                Me.Hide()
+                MsgBox("Bienvenido usuario")
+                conexion.Close()
 
-            Label1.Text = "Conexion establecida"
+            ElseIf tipo_usu = "ADMIN" Then
+                Hotel_El_paso_inicio.Show()
+                MsgBox("Bienvenido administrador")
+                Me.Hide()
+                conexion.Close()
+            End If
+
+        Else
+            MsgBox("Los datos ingresados son incorrectos, intente nuevamente")
+
             conexion.Close()
 
         End If
-
-    End Sub
-
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles ingresar.Click
-        Dim consulta As String = "select nombre, clave from usuarios where nombre='" & TextBox1.Text & "' and clave = '" & TextBox2.Text & "'"
     End Sub
 
     Private Sub borrar_Click(sender As Object, e As EventArgs) Handles borrar.Click
