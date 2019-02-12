b7de4ea4017740057348fe0bb6c2768c0baf0986
Fernando Junco <47456887+yokian1@users.noreply.github.com>
2019-02-12 12:17:13 -0400

    Add files via upload

--- HotelElPaso/HotelElPaso/Empleados.Designer.vb
+++ HotelElPaso/HotelElPaso/Empleados.Designer.vb
@@ -25,9 +25,10 @@ Partial Class Empleados
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.ListBox1 = New System.Windows.Forms.ListBox()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label1
@@ -55,14 +56,6 @@ Partial Class Empleados
         Me.TextBox1.Size = New System.Drawing.Size(100, 20)
         Me.TextBox1.TabIndex = 2
         '
-        'ListBox1
-        '
-        Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.Location = New System.Drawing.Point(15, 27)
-        Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(428, 212)
-        Me.ListBox1.TabIndex = 3
-        '
         'Label3
         '
         Me.Label3.AutoSize = True
@@ -81,19 +74,31 @@ Partial Class Empleados
         Me.Button1.Text = "Button1"
         Me.Button1.UseVisualStyleBackColor = True
         '
+        'DataGridView1
+        '
+        Me.DataGridView1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(6, 12)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(457, 218)
+        Me.DataGridView1.TabIndex = 6
+        '
         'Empleados
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(475, 356)
+        Me.Controls.Add(Me.DataGridView1)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.ListBox1)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Name = "Empleados"
         Me.Text = "Empleados"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -101,7 +106,7 @@ Partial Class Empleados
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents Label2 As System.Windows.Forms.Label
     Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
-    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
     Friend WithEvents Label3 As System.Windows.Forms.Label
     Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
 End Class
--- HotelElPaso/HotelElPaso/Empleados.vb
+++ HotelElPaso/HotelElPaso/Empleados.vb
@@ -1,6 +1,29 @@
-﻿Public Class Empleados
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Empleados
 
     Private Sub Empleados_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        'me conecto a la base de datos'
+        Conectar()
+        'llamo al sub llenagrid'
+        llenagrid()
+    End Sub
+    'creo el sub para llenar datagridview1'
+    Private Sub llenagrid()
+        'creamos dataset'
+        Dim ds As New DataSet
+        'creamos datatable'
+        Dim dt As New DataTable
+        'creamos consulta sql'
+        Dim strsql As String = "Select cod_usu, nombre, apellido, CI, correo, tlf, direccion, cargo, salario_bsf from empleados"
+        'creamos un adaptador mysql que necesita la consulta sql y la conexion a la base de datos'
+        Dim adp As New MySqlDataAdapter(strsql, conexion)
+        'creamos una tabla de nombre tabla'
+        ds.Tables.Add("tabla")
+        'el data llena la tabla de nombre "tabla" con la informacion que tiene el adaptador'
+        adp.Fill(ds.Tables("tabla"))
+        'decirle al datagrid que tome los valores de la consulta sql'
+        Me.DataGridView1.DataSource = ds.Tables("tabla")
+        Me.DataGridView1.Columns(0).HeaderText = " Codigo de Usuario"
     End Sub
 End Class
